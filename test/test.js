d723be7d13313f4ca214a8ff2a0fac3d298d2edb
Mgen <m@mgenware.com>
2016-02-22 10:33:09 +0800

    Add test for exit code and stderr

--- test/test.js
+++ test/test.js
@@ -5,11 +5,21 @@ var bluebirdPromise = require('bluebird');
 function runTestsWithPromise(promise, desc) {
     spawnAsync.setPromise(promise);
     describe(desc, () => {
-        it('echo', () => {
+        it('echo: retCode 0, stdout', () => {
             var stdout = '';
-            return spawnAsync(['echo', ['test']], {stdout: (data) => stdout += data}).then((res) => {
+            return spawnAsync(['echo', ['test']], {stdout: data => stdout += data}).then(res => {
                 assert.equal(res, 0);
                 assert.equal(stdout, 'test\n');
+            });
+        });
+
+        it('grep: retCode 2, stderr', () => {
+            var stderr = '';
+            return spawnAsync(['grep', ['find', '_no_such_file_']], {stderr: data => stderr += data}).then(() => {
+                assert(0, 'Process should exit with code 2');
+            }).catch(err => {
+                assert.equal(err.code, 2);
+                assert(stderr);
             });
         });
     });
